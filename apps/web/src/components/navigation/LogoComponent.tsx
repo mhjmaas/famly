87b0365d4dc6ebdc0ee595cf24290facdc1b8b54
mhjmaas <maas.marcel@gmail.com>
2025-11-17 23:45:07 +0100

    fix: lint issue

--- apps/web/src/components/navigation/LogoComponent.tsx
+++ apps/web/src/components/navigation/LogoComponent.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 interface LogoComponentProps {
   showText?: boolean;
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
   testId?: string;
 }
 
